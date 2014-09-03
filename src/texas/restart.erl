ccaf398b20f45ad75b0b811cb4647945677f8e2a
jacy <johnsoncr.hong@gmail.com>
2014-09-03 18:30:16 +0800

    reset pot info when game ends.

--- src/texas/restart.erl
+++ src/texas/restart.erl
@@ -6,6 +6,6 @@
 start(Game, Ctx, []) ->
   	g:broadcast(Game, #notify_end_game{ game = Game#game.gid }),
 	g:broadcast_player_state(Game),
-    {goto, top, Game, Ctx}.
+    {goto, top, Game#game{pot = pot:new()}, Ctx}.
 
 
