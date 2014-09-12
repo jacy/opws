e9f0ea59e0772890008bfb8451e56362b00764cb
jacy <johnsoncr.hong@gmail.com>
2014-09-12 11:18:16 +0800

    set the mnesia dir excliplictly

--- master.sh
+++ master.sh
@@ -1,2 +1,11 @@
+COOKIE='jacygames'
+NODE_NAME='s1@127.0.0.1'
+MNESIA_DIR='/mnesia/'$NODE_NAME
+
+
+echo 'Mnesia storage dir:'$MNESIA_DIR
+sudo mkdir -p $MNESIA_DIR
+sudo chown -R jacy:jacy $MNESIA_DIR
+
 erl -make
-erl -pa ebin  deps/*/ebin  -name s1@127.0.0.1 -setcookie jacygames
+erl -pa ebin  deps/*/ebin  -name $NODE_NAME -setcookie $COOKIE -mnesia dir '"'$MNESIA_DIR'"'
--- slave.sh
+++ slave.sh
@@ -1,2 +1,10 @@
+COOKIE='jacygames'
+NODE_NAME='s2@127.0.0.1'
+MNESIA_DIR='/mnesia/'$NODE_NAME
+
+echo 'Mnesia storage dir:'$MNESIA_DIR
+sudo mkdir -p $MNESIA_DIR
+sudo chown -R jacy:jacy $MNESIA_DIR
+
 erl -make
-erl -pa ebin  deps/*/ebin  -name s2@127.0.0.1 -setcookie jacygames
+erl -pa ebin  deps/*/ebin  -name $NODE_NAME -setcookie $COOKIE -mnesia dir '"'$MNESIA_DIR'"'
