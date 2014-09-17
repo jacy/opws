8bbf94ec5bad9534ff88d027a48a391b72adabc3
jacy <johnsoncr.hong@gmail.com>
2014-09-17 18:20:12 +0800

    update comments.

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 echo "Compiling...."
 erl -make
 echo '==================================='
-echo 'Start Mnesia Master: m1@127.0.0.1'
+echo 'Set up Mnesia Master: m1@127.0.0.1'
 ./start.sh m1 "cluster mnesia_master"
-echo '==================================='
+echo 'Waitting...'
 sleep 2
-echo 'Start Lobby: lobby@127.0.0.1'
+echo 'Mnesia Master Was Started'
+echo '==================================='
+echo 'Set up Lobby: lobby@127.0.0.1'
 ./start.sh lobby "cluster setup_lobby m1@127.0.0.1"
 echo '==================================='
-echo 'Start Game: game@127.0.0.1'
+echo 'Set up Game: game@127.0.0.1'
 ./start.sh game "cluster setup_games m1@127.0.0.1"
 echo '==================================='
--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #Param1=nodeName, Param2=init script
 
 NODE_NAME="$1@127.0.0.1"
-echo "	Node name is:$NODE_NAME"
+echo "	Start node: $NODE_NAME"
 
 COOKIE="jacygames"
 MNESIA_DIR="/mnesia/$NODE_NAME"
 LOG_DIR="/tmp/elog"
 
-echo "	Mnesia storage dir:$MNESIA_DIR"
+echo "	Mnesia storage dir: $MNESIA_DIR"
 sudo mkdir -p $MNESIA_DIR
 sudo chown -R jacy:jacy $MNESIA_DIR
 
 sudo mkdir -p $LOG_DIR
 sudo chown -R jacy:jacy $LOG_DIR
 
-erl -pa ebin  deps/*/ebin  -name $NODE_NAME -setcookie $COOKIE -mnesia dir '"'$MNESIA_DIR'"' -s $2 -detached -init_debug
+erl -pa ebin  deps/*/ebin  -name $NODE_NAME -setcookie $COOKIE -mnesia dir '"'$MNESIA_DIR'"' -s $2 -detached
